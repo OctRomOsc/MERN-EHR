e137212ff8566c5982c6b47151b3cc4c1630dc29
Oct <rcabrera@outlook.com>
2025-04-11 13:28:16 -0400

    first commit - project setup

---
+++ backend/src/server.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import rateLimit from 'express-rate-limit'
+const app = express();
+const port = 3000;
+
+// Set up rate limiter: maximum of 100 requests per 15 minutes per IP
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    message: "Too many requests from this IP, please try again after 15 minutes",
+  });
+
+app.use(limiter);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  return console.log(`Express is listening at http://localhost:${port}`);
+});
+
